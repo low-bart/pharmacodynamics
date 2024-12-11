a57db5d61601fa5f51860f7e3d0bfa6a4524790c
Lowell Bartlett <bartlett.lowell@gmail.com>
2024-12-11 10:07:57 -0800

    Add metadata class
    Shared between WellData and DrugReports

---
+++ PharmOps/__main__.py
@@ -0,0 +1,10 @@
+import bindMods
+from guitools import *
+
+if __name__ == "__main__":
+    # make GUI window appear
+    sampleWellPlate = 'sample data\\Binding Template for RAW transformations.xlsx'
+    initialize_GUI(sampleWellPlate)
+    # load directories of h5 or xlsx files
+    # make new DrugReports
+    
--- PharmOps/bindMods.py
+++ PharmOps/bindMods.py
@@ -1,30 +1,66 @@
+import numpy as np
+
+class AssayMetadata:
+    drug = []
+    date = []
+    ctr = []
+    plate = []
+
+    def __init__(self):
+        pass
+
 class WellData:
 
-    date = []       # assay date
     ctr = []        # color to right
     plate = []      # plate number
-    drugs = []      # list of drugs on plate, one per row
+    drugs = []      # list of drugs on plate, one per two rows
     comments = []   # list
-    data = []
+    data = []       # well plate data
+    totals = []     # well plate totals with no drug
+    nsb = []        # well plate non-specific binding
+    metadata = AssayMetadata()
 
     def __init__(self, df):
         self.data = df.loc[df.index[0:8], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]]
+        self.drugs = ["Drug1", "Drug2", "Drug3", "Drug4"]
+        plateTotals = self.data.loc[self.data.index[0:8], [1, 12]]
+        self.totals = np.average(plateTotals)
+        plateNSB = self.data.loc[self.data.index[0:8], 2]
+        self.nsb = np.average(plateNSB) 
 
     def display(self):
         print(self.data)
         print(self.comments)
+        print(self.drugs)
 
     def add_comment(self, comment):
         self.comments.append(comment)
 
-    def make_drug_report(self, row):
-        return DrugReports([], [])
+    def make_drug_report(self, index):
+        return DrugReports(self, index)
+
 
 class DrugReports:
-    drug = []
+    average = []
+    specific = []
     concentration = []
+    logConc = []
+    pctTotal = []
+    averageSpecific = []
+    specificBound = []
     pctSpecificBinding = []
-    receptor = []
+    metadata = AssayMetadata()
 
-    def __init__(self, wellRow, info):
-        pass
+    def __init__(self, wellData, index):
+        self.metadata.drug = wellData.drugs[index]
+        self.metadata.date = wellData.date
+        self.metadata.ctr = wellData.ctr
+        self.metadata.plate = wellData.plate
+        # isolate rows by index
+        startRow = (index) * 2
+        endRow = startRow + 2
+        drugData = wellData.data.iloc[startRow:endRow]
+        plateNSB = drugData.loc[drugData.index[0:8], 2]
+        plateTotals = drugData.loc[drugData.index[0:8], [1, 12]]
+        self.averageSpecific = np.average(plateTotals)
+        print(plateTotals)
